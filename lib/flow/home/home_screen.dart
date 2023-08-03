f966e5043ae412a0cccafa4abc78e4ce9c29fe2d
Tomislav Mikšik <tomislav.miksik@gmail.com>
2023-08-04 00:11:29 +0200

    Stopped tracking .env File

---
+++ lib/flow/home/home_screen.dart
@@ -0,0 +1,23 @@
+
+
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+@RoutePage()
+class HomeScreen extends HookConsumerWidget{
+
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeRoute'),
+      ),
+      body: const Center(
+        child: Text('HomeRoute is working'),
+      ),
+    );
+  }
+}
